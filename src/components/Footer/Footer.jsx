68674e6540646743da9aae6c3f56acf2a20f7f14
qtrngn <nguyentrucquynh98@gmail.com>
2025-03-17 17:11:40 -0700

    finished login page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import youtube_icon from "../../assets/youtube_icon.png";
+import twitter_icon from "../../assets/twitter_icon.png";
+import instagram_icon from "../../assets/instagram_icon.png";
+import facebook_icon from "../../assets/facebook_icon.png";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      
+    <div className="footer">
+      <div className="footer-icons">
+        <img src={youtube_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={facebook_icon} alt="" />
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Privacy</li>
+        <li>Help Center</li>
+        <li>Legal Notices</li>
+        <li>Term of Use</li>
+        <li>Media Center</li>
+        <li>Cookies Preferences</li>
+        <li>Ad Choices</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+        <li>Gift Cards</li>
+      </ul>
+      <p className="copyright-text">@ 1997-2025 Netflix, Inc.</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;                                                
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
+import logo from "../../assets/logo.png";
 
 const Login = () => {
   return (
-    <div className='login'>
-      
+    <div className="login">
+      <img src={logo} className="login-logo" alt="" />
+      <div className="login-form">
+        <h1>Sign In</h1>
+        <form>
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign In</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Helps?</p>
+          </div>
+        </form>
+        <div className="sign-up">
+          <p>New to Netflix?</p><p>Sign Up</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
